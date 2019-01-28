31eca68722bd0d1d0e41587f86856038f627419b
Yan <yan.cborges@gmail.com>
2019-01-28 16:30:27 -0200

    Columns changes
    some comm ands for manipulating columns with pandas

---
+++ pandas/manipulate_columns.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+
+ufo = pd.read_csv('http://bit.ly/uforeports')
+
+ufo.columns
+# Returns a list with all column names
+
+ufo.rename(columns = {'Colors Reported':'Colors_Reported', 'Shape Reported':'Shape_Reported'}, inplace=True)
+# Looks for the first argument in columns, and rename for the second argument
+# inplace = If true returns a new dataframe, not a copy
+
+ufo_cols = ['city','colors reported','shape reported','state','time']
+ufo.columns = ufo_cols
+# Overwrittes the name of the dataframe columns with the values in the list
+
+ufo = pd.read_csv('http://bit.ly/uforeports', names=ufo_cols, header=0)
+# Reads the file with that list as header name
+# header = 0: The file does contain a row with header name. This argument ignores it
+
