25e465bb4a3bfd294c9c7dee15a63eab99f75b2a
Yan <yan.cborges@gmail.com>
2019-01-31 17:55:42 -0200

    Japanese analyser
    - Dictionary changed to csv format
    - Improved sentence splitter
    - Started to tokenize words

--- NLTK/japanese/japanese_splitter.py
+++ NLTK/japanese/japanese_splitter.py
@@ -1,8 +1,8 @@
 import writter
-
+import pandas as pd
 
 # Splitting by punctuation
-def sentenceSplit(text, p):
+def jSentence_tokenize(text, p):
 	start = 0
 	s_list = []
 	count = 0
@@ -14,7 +14,12 @@ def sentenceSplit(text, p):
 		count += 1
 	return s_list
 
-# Main takenizer function. Splits chars only when the word is complete
+def check_word(text, d):
+	if(text in d):
+		return True
+	return False
+
+# Main tokenizer function. Splits chars only when the word is complete
 def jTokenize(text):
 
 	main_response = []
@@ -26,10 +31,42 @@ def jTokenize(text):
 		katakanaData = k.read()
 	with open('punct.txt', 'r', encoding='utf8') as p:
 		punctData = p.read()
+	dictData = pd.read_csv('dictionary.txt')
 
 	# Splitting by sentences
+	sentences = jSentence_tokenize(text, punctData)
 	
-	sentences = sentenceSplit(text, punctData)
-	return sentences
+	# Splitting each sentence by its words
+	for sent in sentences:
+		sent_tokenized = []
+		char_string = ''
+		# If the code does not recognize the parameter as a single word, the next char
+		# is appended to it, until the word ends
+		count = 0
+		stopped = 0
+		while(count < len(sent)):
+			char_string += sent[count]
+			if(check_word(char_string, list(dictData.Word))):
+				sent_tokenized.append(char_string)
+				char_string = ''
+			else:
+				if(len(char_string) == 1):
+					stopped = count+1
+			count += 1
+			if(count >= len(sent)):
+				count = stopped
+				char_string = ''
+			#print(count)
+		'''		
+		for char in sent:
+			char_string += char
+			if(check_word(char_string, list(dictData.Word))):
+				sent_tokenized.append(char_string)
+				char_string = ''
+		'''
+		main_response.append(sent_tokenized)
+
+	return main_response
+
 
 	
