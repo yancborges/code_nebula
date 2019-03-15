88401c1215c46bd3370be505b4cc1821e3c910c3
Yan <yan.cborges@gmail.com>
2019-03-15 16:27:04 -0300

    some string functions

---
+++ PHP/string_funcs.php
@@ -0,0 +1,15 @@
+<?php
+
+	$output = substr('START ATE 1 but ends ate 5 please', 1, 5);
+	$output = srtlen('How long is this???');
+	$output = strpos('Find a number 7 here for me.', '7');
+	$output = strrpos('Now find the last 7 here for my, not the first 7.', '7');
+	$output = trim('She said: I need more space');
+	$output = strtoupper('my father said that i should grow up');
+	$output = strtolower('I wish i could play at seesaw');
+	$output = ucwords('make it polite');
+	$output = str_replace('meat', 'salad', 'I always eat meat');
+	$output = is_string('Am i a string? Is this real life?');
+	
+
+?>
