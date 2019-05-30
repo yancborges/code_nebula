aa14e6dcf015e7d456b93cbfc32679d4a209a9fb
Yan <yan.cborges@gmail.com>
2019-05-29 22:27:09 -0300

    more matrix math

--- deep learning/matrix_math.py
+++ deep learning/matrix_math.py
@@ -1,3 +1,5 @@
+import random
+
 class Matrix:
 
 	def __init__(self, rows, cols):
@@ -13,11 +15,38 @@ class Matrix:
 
 
 	def mult(self, n):
-		for i in range(self.rows):
-			for j in range(self.cols):
-				self.matrix[i][j] *= n
+		if(type(n) == type(self)):
+			if(self.rows == n.rows and self.cols == n.cols):
+				for i in range(self.rows):
+					for j in range(self.cols):
+						self.matrix[i][j] *= n.matrix[i][j]
+			elif(self.rows == n.cols and self.cols == n.rows):
+				new_matrix = Matrix(self.rows, n.cols)
+				for i in range(new_matrix.rows):
+					for j in range(new_matrix.cols):
+						s = 0
+						for k in range(self.matrix.cols):
+							s += self.matrix[i][k] * n.matrix[k][j]
+						new_matrix.matrix[i][j] = s
+				return new_matrix
+
+
+		elif(type(n) == 'int'):
+			for i in range(self.rows):
+				for j in range(self.cols):
+					self.matrix[i][j] *= n
 
 	def add(self, n):
+		if(type(n) == type(self)):
+			for i in range(self.rows):
+				for j in range(self.cols):
+					self.matrix[i][j] += n.matrix[i][j]
+		elif(type(n) == 'int'):
+			for i in range(self.rows):
+				for j in range(self.cols):
+					self.matrix[i][j] += n
+
+	def randomize(self):
 		for i in range(self.rows):
 			for j in range(self.cols):
-				self.matrix[i][j] += n
+				self.matrix[i][j] *= random.randint(0,9)
